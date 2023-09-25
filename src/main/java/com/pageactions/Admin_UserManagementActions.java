2fb5fbd94ae6153f561ecc94d8121f4b42ee2a7d
Mallikarjun-Maralinganavar <141898201+Mallikarjun-Maralinganavar@users.noreply.github.com>
2023-09-25 15:54:56 +0530

    1.0
    1.0

---
+++ src/main/java/com/pageactions/Admin_UserManagementActions.java
@@ -0,0 +1,47 @@
+package com.pageactions;
+
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.support.PageFactory;
+
+import com.pageobjects.Admin_UserManagementObj;
+import com.utils.DriverClass;
+
+public class Admin_UserManagementActions {
+	Admin_UserManagementObj userManagementObj = null;
+	
+	public Admin_UserManagementActions() {
+		// TODO Auto-generated constructor stub
+		this.userManagementObj = new Admin_UserManagementObj();
+		
+		PageFactory.initElements(DriverClass.getDriver(), userManagementObj);
+	}
+		
+	public void setAdmin() {
+		userManagementObj.adminOp.click();
+	}
+	
+	public void setUserManagement() {
+		userManagementObj.userMangement.click();
+	}
+	
+	public void setUser() {
+		userManagementObj.users.click();
+	}
+	
+	public void setAdd() {
+		userManagementObj.add.click();
+	}
+	
+	public void setAddUser() throws InterruptedException {
+		userManagementObj.selectuserRole.sendKeys(Keys.ARROW_DOWN,Keys.ENTER);
+		userManagementObj.employeeName.sendKeys("a");
+		Thread.sleep(2000);
+		userManagementObj.employeeName.sendKeys(Keys.ARROW_DOWN,Keys.ENTER);
+		userManagementObj.userName.sendKeys("Bug Striker");
+		userManagementObj.selectstatus.sendKeys(Keys.ARROW_DOWN,Keys.ENTER);
+		userManagementObj.passWord.sendKeys("admin123");
+		userManagementObj.confirmPassword.sendKeys("admin123");
+		
+	}
+	
+}
---
+++ src/main/java/com/pageactions/OrangeHRMLoginPageActions.java
@@ -0,0 +1,47 @@
+package com.pageactions;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
+import org.openqa.selenium.support.PageFactory;
+
+import com.pageobjects.OrangeHRMObj;
+import com.utils.DriverClass;
+
+public class OrangeHRMLoginPageActions {
+	OrangeHRMObj loginPageLocators = null;
+	String strUserName, strPassWord;
+	
+	public OrangeHRMLoginPageActions() {
+		this.loginPageLocators = new OrangeHRMObj();
+		
+		PageFactory.initElements(DriverClass.getDriver(), loginPageLocators);
+	}
+	
+	public void setUserName(String usname) {
+		loginPageLocators.UserName.sendKeys(usname);
+	}
+	
+	public void setPassWord(String psw) {
+		loginPageLocators.passWord.sendKeys(psw);
+	}
+	
+	public void setLoingbtn() {
+		loginPageLocators.clickBtn.click();
+	}
+	
+	public void setLogOut() {
+		loginPageLocators.clikProfile.click();
+		loginPageLocators.clickLogout.click();
+	}
+	
+	public void login(String userName,String passWord) {
+	    
+	    this.setUserName(userName);
+	    this.setPassWord(passWord);
+	}
+	
+}
