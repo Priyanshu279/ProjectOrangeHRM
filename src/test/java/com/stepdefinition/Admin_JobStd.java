a9b0bd5b65140afe93de884fdbb3d084c5b4d931
Mallikarjun-Maralinganavar <141898201+Mallikarjun-Maralinganavar@users.noreply.github.com>
2023-09-25 15:20:49 +0530

    Udate 1.0
    1.0

---
+++ src/test/java/com/stepdefinition/Admin_JobStd.java
@@ -0,0 +1,57 @@
+package com.stepdefinition;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class Admin_JobStd {
+	
+	@Given("Select Job and click on Job Title")
+	public void select_job_and_click_on_job_title() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+	
+	@When("Click on Add and filling the details")
+	public void click_on_add_and_filling_the_details() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+	
+	@When("Click on Save Job Title")
+	public void click_on_save_Job_Title() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("User validate the saved Successfully")
+	public void user_validate_the_saved_successfully() {
+    // Write code here that turns the phrase above into concrete actions
+    throw new io.cucumber.java.PendingException();
+	}
+	
+	@Given("Select Job and click on Pay Grades")
+	public void select_job_and_click_on_pay_grades() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("Click on Save Pay Grades")
+	public void click_on_save_pay_grades() {
+    // Write code here that turns the phrase above into concrete actions
+    throw new io.cucumber.java.PendingException();
+	}
+	
+	@When("Successfully added go for search and and the delete the user")
+	public void successfully_added_go_for_search_and_and_the_delete_the_user() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+	
+	@Then("Validate weather the user deleted Successfully")
+	public void validate_weather_the_user_deleted_successfully() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+	
+}
