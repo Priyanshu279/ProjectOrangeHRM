777bb5502ee323caae97349372a10d7aebe3a309
Priyanshu Suryavanshi <86827550+Priyanshu279@users.noreply.github.com>
2023-09-25 17:23:27 +0530

    main branch

---
+++ ProjectOrangeHRM/ProjectOrangeHRM/src/test/java/com/stepdefinition/Admin_HelpStd.java
@@ -0,0 +1,20 @@
+package com.stepdefinition;
+
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class Admin_HelpStd {
+
+	@When("Click on Help")
+	public void click_on_help() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("validate Help function working fine or not")
+	public void validate_help_function_working_fine_or_not() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+}
